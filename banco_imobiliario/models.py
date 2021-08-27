ee909dc73f36c598d04304f033c1157aeb0f67cb
Delatorrea <emerson@delatorre.dev>
2021-08-27 18:50:23 -0300

    Implementadas regras do jogo

--- banco_imobiliario/models.py
+++ banco_imobiliario/models.py
@@ -1,105 +1,51 @@
-from abc import abstractclassmethod, abstractmethod
+from estrutura import Propriedade, Tabuleiro
+from abc import abstractmethod
 from enum import Enum
 import random
 
 
-
-class Tabuleiro:
-    def __init__(self, propriedades: list) -> None:
-        self.__propriedades = propriedades
-    
-    def quantidadeDeCasas(self) -> int:
-        return len(self.__propriedades)
-    
-    def retornarPropriedade(self, casa: int) -> object:
-        return self.__propriedades[casa-1]
-
-
-
-class Dado:
-    def __init__(self) -> None:
-        self.__lado = 0
-    
-    def sortear(self) -> int:
-        self.__lado = random.randint(1, 6)
-
-        return self.__lado
-
-
-
-class Propriedade:
-    def __init__(self, custo_venda, valor_aluguel, posicao) -> None:
-        self.__custo_venda = custo_venda
-        self.__valor_aluguel = valor_aluguel
-        self.__proprietario = None
-
-    def disponivel(self) -> bool:
-        if self.__proprietario is None:
-            return True
-
-        return False
-    
-    def retornarValorAluguel(self) -> int:
-        return self.__valor_aluguel
-    
-    def retornarCustoVenda(self) -> int:
-        return self.__custo_venda
-    
-    def retornarProprietario(self) -> object:
-        return self.__proprietario
-
-    def definirNovoProprietario(self, proprietario: object) -> bool:
-        if self.disponivel:
-            self.__proprietario = proprietario
-            return True
-
-        return False
-        
-
-
 class Jogador:
-    def __init__(self, tipo: Enum = 0) -> None:
-        self.__casa = 0
-        self.__saldo = 300
-        self.__tipo = tipo
+    def __init__(self, tabuleiro: Tabuleiro, tipo: Enum) -> None:
+        self._tabuleiro = tabuleiro
+        self._casa = 0
+        self._saldo = 300
+        self._tipo = tipo
 
-    def avancarCasas(self, dado: int, qtd_casas: int) -> int:
-        self.__resultado = self.__casa + dado
+    def avancar_casas(self, dado: int) -> int:
+        _resultado = self._casa + dado
 
-        if self.__resultado > qtd_casas:
-            self.__casa = self.__resultado - qtd_casas
+        if _resultado > self._tabuleiro.quantidade_de_casas():
+            self._casa = _resultado - self._tabuleiro.quantidade_de_casas()
         else:
-            self.__casa = self.__resultado
-
-        return self.__casa
+            self._casa = _resultado
+        return self._casa
     
-    def retornarSaldo(self) -> int:
-        return self.__saldo
+    def retornar_saldo(self) -> int:
+        return self._saldo
 
     @abstractmethod
-    def analisar(self):
+    def analisar(self, propriedade: Propriedade):
         pass
 
     def _comprar(self, propriedade: Propriedade):
-        self.__saldo -= propriedade.retornarCustoVenda()
-        propriedade.definirNovoProprietario(self)
+        self._saldo -= propriedade.retornar_custo_venda()
+        propriedade.definir_novo_proprietario(self)
     
     def _pagar(self, propriedade: Propriedade):
-        self.__saldo -= propriedade.retornarValorAluguel()
-        proprietario = propriedade.retornarProprietario()
-        proprietario._receber(propriedade.retornarValorAluguel())
+        self._saldo -= propriedade.retornar_valor_aluguel()
+        proprietario = propriedade.retornar_proprietario()
+        proprietario.receber(propriedade.retornar_valor_aluguel())
     
-    def _receber(self, valor: int):
-        self.__saldo += valor
+    def receber(self, valor: int):
+        self._saldo += valor
     
     def __str__(self):
-        return f'Jogador( Tipo: {self.__tipo.name}, Saldo: {self.__saldo} )'
+        return f'Jogador( Tipo: {self._tipo.name}, Saldo: {self._saldo} )'
         
 
-
 class Impulsivo(Jogador):
-    def __init__(self) -> None:
-        super().__init__(ETipoJogador.Impulsivo)
+    def __init__(self, tabuleiro) -> None:
+        super().__init__(tabuleiro, ETipoJogador.Impulsivo)
     
     def analisar(self, propriedade: Propriedade):
         if propriedade.disponivel():
@@ -108,36 +54,33 @@ class Impulsivo(Jogador):
             self._pagar(propriedade)
 
 
-
 class Exigente(Jogador):
-    def __init__(self) -> None:
-        super().__init__(ETipoJogador.Exigente)
+    def __init__(self, tabuleiro) -> None:
+        super().__init__(tabuleiro, ETipoJogador.Exigente)
 
     def analisar(self, propriedade: Propriedade):
         if propriedade.disponivel():
-            if propriedade.retornarValorAluguel() > 50:
+            if propriedade.retornar_valor_aluguel() > 50:
                 self._comprar(propriedade)
         else:
             self._pagar(propriedade)
-
 
 
 class Cauteloso(Jogador):
-    def __init__(self) -> None:
-        super().__init__(ETipoJogador.Cauteloso)
+    def __init__(self, tabuleiro) -> None:
+        super().__init__(tabuleiro, ETipoJogador.Cauteloso)
 
     def analisar(self, propriedade: Propriedade):
         if propriedade.disponivel():
-            if self.retornarSaldo() - propriedade.retornarCustoVenda() >= 80:
+            if self.retornar_saldo() - propriedade.retornar_custo_venda() >= 80:
                 self._comprar(propriedade)
         else:
             self._pagar(propriedade)
 
 
-
 class Aleatorio(Jogador):
-    def __init__(self) -> None:
-        super().__init__(ETipoJogador.Aleatorio)
+    def __init__(self, tabuleiro) -> None:
+        super().__init__(tabuleiro, ETipoJogador.Aleatorio)
 
     def analisar(self, propriedade: Propriedade):
         if propriedade.disponivel():
@@ -145,7 +88,6 @@ class Aleatorio(Jogador):
                 self._comprar(propriedade)
         else:
             self._pagar(propriedade)
-
 
 
 class ETipoJogador(Enum):
