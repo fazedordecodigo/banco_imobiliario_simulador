8bab6c20c519a83641d6f02e235bca62306a5239
Delatorrea <emerson@delatorre.dev>
2021-08-28 23:46:59 -0300

    Adicionado metodo Setup

--- tests/test_configuracao.py
+++ tests/test_configuracao.py
@@ -6,36 +6,34 @@ import unittest
 
 class TestConfiguracao(TestCase):
 
+    def setUp(self) -> None:
+        self.configuracao = Configuracao()
+        self.tabuleiro = self.configuracao.montar_tabuleiro()
+
     def test_configuracao_deve_retornar_tabuleiro(self):
-        configuracao = Configuracao()
+
         self.assertIsInstance(
-            configuracao.montar_tabuleiro(),
+            self.tabuleiro,
             Tabuleiro,
             'montar_tabuleiro não retorna um Tabuleiro'
         )
 
     def test_tabuleiro_deve_ter_20_casas(self):
-        configuracao = Configuracao()
-        tabuleiro = configuracao.montar_tabuleiro()
-        self.assertEqual(tabuleiro.quantidade_de_casas(), 20)
+        self.assertEqual(self.tabuleiro.quantidade_de_casas(), 20)
 
     def test_configuracao_deve_retornar_dado(self):
-        configuracao = Configuracao()
         self.assertIsInstance(
-            configuracao.retorna_dado(),
+            self.configuracao.retorna_dado(),
             Dado,
             'retornar_dado não retorna um Dado'
         )
 
     def test_configuracao_deve_retornar_lista_jogadores(self):
-        configuracao = Configuracao()
-        tabuleiro = configuracao.montar_tabuleiro()
         jogadores_esperado = [
-            Impulsivo(tabuleiro), Exigente(tabuleiro),
-            Cauteloso(tabuleiro), Aleatorio(tabuleiro)
+            Impulsivo(self.tabuleiro), Exigente(self.tabuleiro),
+            Cauteloso(self.tabuleiro), Aleatorio(self.tabuleiro)
         ]
-
-        jogadores = configuracao.retorna_jogadores()
+        jogadores = self.configuracao.retorna_jogadores()
 
         self.assertListEqual(
             list(map(vars, jogadores_esperado)),
